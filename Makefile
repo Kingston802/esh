39583af00b92b2deef6b2740302436b21f2fdffd
Kingston802 <alexkingstonbailey@gmail.com>
2021-04-01 21:43:09 +1300

    created a basic main  and makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+CC=gcc
+CXX=g++
+RM=rm -f
+CPPFLAGS=-g -Wall -O2
+
+SRCS=main.cpp 
+
+all: run 
+	
+run:
+	$(CXX) $(CPPFLAGS) $(SRCS)
+
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    const char *introAscii = "\n"            
+"            _       \n"
+"           | |      \n"
+"   ___  ___| |__    \n"
+"  / _ \\/ __| '_ \\   \n"
+" |  __/\\__ \\ | | |  \n"
+"  \\___||___/_| |_|  \n"
+"                    \n";                  
+    printf("%s", introAscii);
+}
